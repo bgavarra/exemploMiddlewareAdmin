119ac1fa338f7136fb26d42ead820fd0afd257a6
Bruno Gavarra <brunogavarra@ejcm.com.br>
2018-02-05 11:35:46 -0200

    exemplo middleware admin

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -47,6 +47,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+        'checkAdmin' => \App\Http\Middleware\checkAdmin::class,
         'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,19 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+Route::get('home', 'HomeController@index');
 
-Route::get('/produtos', 'ProductsController@index')->name('produtos');
+Route::get('produtos', 'ProductsController@index')->name('produtos');
 
 
-Route::group(['middleware' => 'auth',], function()
+Route::group(['middleware' => 'checkAdmin',], function()
 {
     //é possível ver as rotas que este método cria usando o comando 'php artisan route:list'  (maximizar a janela do terminal antes de rodar o comando para visualizar a lista corretamente)
     Route::resource('estudantes', 'StudentController');
 });
 
+// Route::group(['before' => 'auth',]), function()
+// {
+//
+//     Route::controller('artigos', 'ArtigosController');
+// });
